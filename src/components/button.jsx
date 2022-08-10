89cddd317c252753f5f370176761b6d05e0e24d2
Artemy <artegoser@gmail.com>
2022-08-10 19:57:11 +0300

    fix: buttons animation

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,7 @@
 function Button(props) {
   return (
     <a href={props.href}>
-      <div className="transition w-48 ease-[cubic-bezier(1,.22,.32,1.69)] delay-60 hover:scale-110 p-2 pl-6 text-lg bg-zinc-400 hover:bg-zinc-600 dark:bg-zinc-600 dark:hover:bg-zinc-800 rounded-2xl">
+      <div className="transition-transform w-48 ease-[cubic-bezier(.69,.58,.32,1.69)] delay-60 hover:scale-105  p-2 pl-6 text-lg bg-zinc-400 hover:bg-zinc-600 dark:bg-zinc-600 dark:hover:bg-zinc-800 rounded-2xl">
         {props.children}
       </div>
     </a>
